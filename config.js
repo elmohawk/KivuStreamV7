a7c9d6e22950085c9674f2c6a3f96edaebd0177e
elmohawk <mrhawk2007@gmail.com>
2026-06-28 17:04:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const TMDB_API_KEY = "8b8937bf3e114fa3502358a4f090c0df";
 
 const TMDB_IMAGE = "https://image.tmdb.org/t/p/original";
-
+const WORKER_URL = "https://kivustream.mrhawk2007.workers.dev/";
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
