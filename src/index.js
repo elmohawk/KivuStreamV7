c6d99437c49a947586559cb14199c4a0b85c8a4f
elmohawk <mrhawk2007@gmail.com>
2026-06-28 15:42:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+export default {
 
+    async fetch(request, env) {
+
+        const url = new URL(request.url);
+
+        if (url.pathname === "/") {
+
+            return Response.json({
+
+                status: "online",
+
+                api: "KivuStream"
+
+            });
+
+        }
+
+        return new Response("Not Found", {
+
+            status: 404
+
+        });
+
+    }
+
+}
