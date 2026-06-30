c7aa2bf3e7192e632b45fa79603cdd9afb1352fb
elmohawk <mrhawk2007@gmail.com>
2026-06-30 11:26:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,23 @@
+"use strict";
 
+/*=========================================
+        KIVUSTREAM CONFIG
+=========================================*/
+
+const SUPABASE_URL = "https://exjgejujfxejjlbfizgz.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
+
+const TMDB_API_KEY = "8b8937bf3e114fa3502358a4f090c0df";
+
+export const WORKER_URL =
+"https://kivustream.mrhawk2007.workers.dev";
+
+export const TMDB_IMAGE =
+"https://image.tmdb.org/t/p/original";
+
+export const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
