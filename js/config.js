c9e860aa0377d5e84b7f8435f101574af7b0413f
elmohawk <mrhawk2007@gmail.com>
2026-07-02 09:19:35 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,9 +8,6 @@
 const SUPABASE_URL = "https://exjgejujfxejjlbfizgz.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
 
-export const WORKER_URL =
-    "https://kivustream.mrhawk2007.workers.dev";
-
 export const supabaseClient =
     supabase.createClient(
         SUPABASE_URL,
